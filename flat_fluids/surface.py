d458fbc2a3b2d4b6d368e09ddc5bc7315b35431c
Lawrence Pryn <lawrencempryn@gmail.com>
2022-05-18 18:40:45 +0100

    Fix type check for 2D array index

--- flat_fluids/surface.py
+++ flat_fluids/surface.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 
 class DecimalFraction(float):
-    def __new__(cls, value: float) -> None:
+    def __new__(cls, value: float) -> float:
         # Ensure value is valid
         if value < 0:
             raise ValueError("Value is less than zero.")
@@ -15,12 +15,26 @@ class DecimalFraction(float):
         return super().__new__(cls, value)
 
 
+class Array2DIndex(tuple):
+    def __new__(cls, value: tuple) -> tuple:
+        # Ensure value is valid
+        if len(value) != 2:
+            raise ValueError("Value must be contain 2 elements.")
+        if not all(isinstance(element, int) for element in value):
+            raise ValueError("Value must contain only ints.")
+        if any(element < 0 for element in value):
+            raise ValueError("Value must only contain positive numbers.")
+        
+        # Run parent super function
+        return super().__new__(cls, value)
+
+
 class Grid(object):
-    def __init__(self, shape: tuple([int, int])) -> None:
+    def __init__(self, shape: Array2DIndex) -> None:
         # Save parameters
         self.shape = shape
 
-    def __is_in(self, position: tuple([int, int])) -> bool:
+    def __is_in(self, position: Array2DIndex) -> bool:
         """
         Check to see if the provided position is within the shape of the grid.
         """
@@ -48,9 +62,25 @@ class Image(Grid):
         self.filepath = filepath
         self.image = __image
         
-    def _surface_mask(self, position: tuple([int, int]),
+    # def __colour_diff()
+        
+    def _surface_mask(self, position: Array2DIndex,
                       tolerance: DecimalFraction) -> np.ndarray:
-        pass
+        """
+        Generate a mask of Booleans to indicate connected pixels in the image
+        with a colour similar to that of a selected pixel. The tolerance
+        provided dictates the level of similarity required between these
+        colours for them to be considered connected.
+        """
+        
+        # Ensure the pixel position is within the image
+        if not self.__is_in(position):
+            raise ValueError("Invalid position selected in the image.")
+        
+        # Determine the selected colour
+        colour = self.image[position]
+        
+        
     
     def plot(self) -> None:
         """
@@ -74,3 +104,6 @@ if __name__ == "__main__":
 
     # Plot
     image.plot()
+    
+    # Determine position of fluid
+    image._surface_mask((0, 0), 0.1)
