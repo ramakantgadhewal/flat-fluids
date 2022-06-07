7ad3a49a021c0acd1fed3e0c10fbea49b63e9f91
Lawrence Pryn <lawrencempryn@gmail.com>
2022-06-07 16:29:53 +0100

    Add 2D array class

--- flat_fluids/fluid.py
+++ flat_fluids/fluid.py
@@ -3,14 +3,34 @@ import surface as sf
 
 
 class Array2D(object):
-    def __new__(cls, value: np.ndarray) -> np.ndarray:
-        # Ensure value is valid
+    def __init__(self, value: np.ndarray, dx: float, dy: float) -> None:
+        # Ensure parameters are valid
         if len(np.shape(value)) != 2:
             raise ValueError("Value must contain two dimensions.")
+        if (dx <= 0) or (dy <= 0):
+            raise ValueError("Cell lengths must be a real positive number.")
+        
+        # Save parameters
+        self.value = value
+        self.dx = dx
+        self.dy = dy
+        
+        # Determine additional parameters
+        self.shape = np.shape(value)
 
-        # Return array
-        return value
+    def x_gradient(self) -> np.ndarray:
+        """
+        Calculates the gradient of the 2D array in the x dimension.
+        """
+        
+        return np.gradient(self.value, self.dx, axis=0)
 
+    def y_gradient(self) -> np.ndarray:
+        """
+        Calculates the gradient of the 2D array in the y dimension.
+        """
+        
+        return np.gradient(self.value, self.dy, axis=1)
 
 class Fluid(object):
     def __init__(self, grid: sf.Grid, gamma: float) -> None:
